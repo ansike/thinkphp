748c29e5181ee6b8bf67ec0028ab7565bb4f8f8f
ansike <ansike@zuoyebang.com>
2018-07-08 18:19:41 +0800

    使用类方法和助手方法读取配置项

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,12 +1,23 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
+use think\Config;
 class Index extends Controller
 {
     public function index()
     {
-        \think\Config::parse(APP_PATH.'../config/newconf/conf.ini','ini');
-        dump(\think\Config::get());
+        //读取其他位置的配置文件
+        // \think\Config::parse(APP_PATH.'../config/newconf/conf.ini','ini');
+        $conf = 'twice.params';
+        $isExist = Config::has($conf);
+        if($isExist){
+            dump(Config::get($conf));
+        }else{
+            return $conf.'配置项不存在';
+        }
+        // dump(Config::get('twice.param'));
+        // dump(\think\Config::has('twice.param'));
+        // dump(config('?app_status'));
         // return 'phps';
     }
 
--- config/index/config.php
+++ config/index/config.php
@@ -1,7 +1,13 @@
-/**
-    模块的默认配置文件 
-*/ 
 <?php
+
+/**
+ * @Author: ansike
+ * @Date:   2018-07-04 17:08:39
+ * @Last Modified by:   ask
+ * @Last Modified time: 2018-07-08 18:00:54
+ */
+
     return [
-        'app_status' => 'office',
+        'app_status' => 'test',
+        'twice' => [ 'param' => 'params' ]
     ];
