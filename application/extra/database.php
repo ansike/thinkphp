972634a24f6e0f7fc506480a98eacb2e0c4db9f0
ansike <ansike@zuoyebang.com>
2018-07-10 00:10:57 +0800

    thinkphp

---
+++ application/extra/database.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @Author: ansike
+ * @Date:   2018-07-08 18:42:54
+ * @Last Modified by:   ask
+ * @Last Modified time: 2018-07-08 18:51:54
+ */
+return [
+    // 用户名
+    'username' => 'roots',
+];
---
+++ application/extra/test.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @Author: ansike
+ * @Date:   2018-07-08 18:53:37
+ * @Last Modified by:   ask
+ * @Last Modified time: 2018-07-08 18:54:10
+ */
+return [
+    'test1' => 'sssasa' 
+];
